b7a57edf4fca3b8eb0a16394676b1b48beb4fd72
joao vieira <john.collins.vieira@gmail.com>
2023-08-26 18:17:02 -0400

    added db.json

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+fetch('https://docs.api.jikan.moe/#tag/anime/operation/getAnimeFullById')
+.then(function(res) {
+    return res.json()
+})
+.then(function(data) {
+    console.log(data)
+})
